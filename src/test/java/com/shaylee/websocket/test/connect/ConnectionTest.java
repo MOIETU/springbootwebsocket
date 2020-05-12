4cf60bf49aafaf58efad41fcab5b4ca40ad64520
Adrian <297513931@qq.com>
2020-05-12 11:14:14 +0800

    :white_check_mark: Adding tests.添加测试用例.

---
+++ src/test/java/com/shaylee/websocket/test/connect/ConnectionTest.java
@@ -0,0 +1,46 @@
+package com.shaylee.websocket.test.connect;
+
+import com.shaylee.websocket.test.TestApplication;
+import com.shaylee.websocket.test.client.WebSocketClient;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {TestApplication.class})
+public class ConnectionTest {
+
+	@Before
+	public void setupContainer() {
+		
+	}
+	
+	@Test
+	public void testConnection() {
+		WebSocketClient client = new WebSocketClient("ws://127.0.0.1:8086/socketServer/UnitTestUser");
+		
+		log.info("创建WebsocketClient后，客户端连接状态:【{}】", client.isOpen());
+		
+		if (!client.isOpen()) {
+			return;
+		}
+		
+		try {
+			Thread.sleep(20000);
+			
+			client.sendTextMessage("测试发送 ABC ... ");
+			
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			log.error("", e);
+		} finally {
+			client.close();
+		}
+	}
+}
