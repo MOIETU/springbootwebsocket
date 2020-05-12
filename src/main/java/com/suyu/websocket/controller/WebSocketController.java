6ed00774c4eab67bf36fdf7df6bf75aea4072301
Adrian <297513931@qq.com>
2020-05-12 10:32:51 +0800

    :art: Improving structure / format of the code.

--- src/main/java/com/suyu/websocket/controller/WebSocketController.java
+++ src/main/java/com/suyu/websocket/controller/WebSocketController.java
@@ -21,8 +21,8 @@ public class WebSocketController {
     private SocketServer socketServer;
 
     /**
-     *
      * 客户端页面
+     *
      * @return
      */
     @RequestMapping(value = "/index")
@@ -32,8 +32,8 @@ public class WebSocketController {
     }
 
     /**
-     *
      * 服务端页面
+     *
      * @param model
      * @return
      */
@@ -42,32 +42,34 @@ public class WebSocketController {
         int num = socketServer.getOnlineNum();
         List<String> list = socketServer.getOnlineUsers();
 
-        model.addAttribute("num",num);
-        model.addAttribute("users",list);
+        model.addAttribute("num", num);
+        model.addAttribute("users", list);
         return "admin";
     }
 
     /**
      * 个人信息推送
+     *
      * @return
      */
     @RequestMapping("sendmsg")
     @ResponseBody
-    public String sendmsg(String msg, String username){
+    public String sendmsg(String msg, String username) {
         //第一个参数 :msg 发送的信息内容
         //第二个参数为用户长连接传的用户人数
-        String [] persons = username.split(",");
-        SocketServer.SendMany(msg,persons);
+        String[] persons = username.split(",");
+        SocketServer.SendMany(msg, persons);
         return "success";
     }
 
     /**
      * 推送给所有在线用户
+     *
      * @return
      */
     @RequestMapping("sendAll")
     @ResponseBody
-    public String sendAll(String msg){
+    public String sendAll(String msg) {
         SocketServer.sendAll(msg);
         return "success";
     }
